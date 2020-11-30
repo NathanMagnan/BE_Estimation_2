018f64c36be01c109100de0ed342a903e9a000f1
NathanMagnan <nathan.magnan@polytechnique.edu>
2020-11-30 21:05:41 +0100

    Modified some images in part 2

--- II_part2.m
+++ II_part2.m
@@ -34,28 +34,28 @@ x_f_1 = abs(G' * x1) / N;
 % ylabel("signal x^{\^}_0");
 
 %% plot the time and frequency representations of the data vector x1
-% figure(1);
-% plot(t, x1);
-% ylim([-3 3]);
-% title("time representation of 5-frequency non-noisy signal");
-% xlabel("time t");
-% ylabel("signal x_0");
-% 
-% figure(2);
-% xline(f_th(1), 'k');
-% plot(freq_grid(m:2 * m + 1), x_f_1(m:2 * m + 1));
-% xlim([0 100]);
-% title("frequency representation of 5-frequency non-noisy signal");
-% xlabel("frequency f");
-% ylabel("signal x^{\^}_0");
-
-%% Plot the spectral window
-Win = abs(G' * ones(N, 1) / N);
+figure(1);
+plot(t, x1);
+ylim([-3 3]);
+title("time representation of 5-frequency noisy signal");
+xlabel("time t");
+ylabel("signal x_0");
 
 figure(2);
 xline(f_th(1), 'k');
-plot(freq_grid, Win);
-xlim([-10 10]);
-title("Spectral window in the irregular sampling case");
+plot(freq_grid(m:2 * m + 1), x_f_1(m:2 * m + 1));
+xlim([0 100]);
+title("frequency representation of 5-frequency noisy signal");
 xlabel("frequency f");
-ylabel("signal");
+ylabel("signal x^{\^}_0");
+
+%% Plot the spectral window
+% Win = abs(G' * ones(N, 1) / N);
+% 
+% figure(2);
+% xline(f_th(1), 'k');
+% plot(freq_grid, Win);
+% xlim([-10 10]);
+% title("Spectral window in the irregular sampling case");
+% xlabel("frequency f");
+% ylabel("signal");
